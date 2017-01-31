474e69c526527dace66a158d64c340fcf23edd9c
박민 <bagmin@bagmin-ui-MacBook-Pro.local>
2017-01-31 13:24:19 +0900

    HotFix

--- GenerateRandomCode.js
+++ GenerateRandomCode.js
@@ -11,10 +11,12 @@ var num = "0123456789";
 export function TextCode( textLength ){
   for( var i=0; i < textLength; i++ )
   {
-      textCode = alphabet.charAt(Math.floor(Math.random() * alphabet.length));
+      textCode += alphabet.charAt(Math.floor(Math.random() * alphabet.length));
   }
 
   result = textCode;
+
+  textCode = "";
 
   return result;
 }
@@ -22,10 +24,12 @@ export function TextCode( textLength ){
 export function NumCode( numLength ){
   for( var i=0; i < numLength; i++ )
   {
-      numCode = num.charAt(Math.floor(Math.random() * num.length));
+      numCode += num.charAt(Math.floor(Math.random() * num.length));
   }
 
   result = numCode;
+
+  numCode = "";
 
   return result;
 }
@@ -34,15 +38,18 @@ export function TextNumCode( textLength, numLength ){
 
    for( var i=0; i < textLength; i++ )
    {
-       textCode = alphabet.charAt(Math.floor(Math.random() * alphabet.length));
+       textCode += alphabet.charAt(Math.floor(Math.random() * alphabet.length));
    }
 
    for( var i=0; i < numLength; i++ )
    {
-       numCode = num.charAt(Math.floor(Math.random() * num.length));
+       numCode += num.charAt(Math.floor(Math.random() * num.length));
    }
 
    result = textCode+numCode;
+
+   textCode = "";
+   numCode = "";
 
    return result;
 }
